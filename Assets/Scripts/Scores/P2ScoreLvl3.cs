7f20038175b38ed83e0faad0f8d7551dd4fd4de2
JeremyAttardd <32672976+JeremyAttardd@users.noreply.github.com>
2017-12-12 18:31:23 +0100

    Update 4

---
+++ Assets/Scripts/Scores/P2ScoreLvl3.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2ScoreLvl3 : MonoBehaviour {
+
+    int Scoree;
+    private LevelManager myLevelManager;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        if (Scoree == 6)
+        {
+            myLevelManager.LoadLevel("Level2");
+        }
+        else
+        {
+            Scoree += 1;
+
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(collision.gameObject.name);
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myLevelManager = GameObject.FindObjectOfType<LevelManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
